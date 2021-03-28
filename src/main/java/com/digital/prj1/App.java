26d4cb6549d01ea9bcfbe4ad568866d30e2ba7f9
ashwin <ashwin.dba12@gmail.com>
2021-03-28 21:01:35 +0000

    maven 1st proj

---
+++ src/main/java/com/digital/prj1/App.java
@@ -0,0 +1,15 @@
+package com.digital.prj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "work like baba" );
+        System.out.println( "24/7  very active and energetic" );
+    }
+}
